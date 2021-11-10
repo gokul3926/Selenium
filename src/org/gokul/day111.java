0bafa63da8fe69165816550b3b5ba3ad47ac3ac8
gokul3926 <gokulmecht90@gmail.com>
2021-11-10 10:15:41 +0530

    second

--- src/org/gokul/day111.java
+++ src/org/gokul/day111.java
@@ -3,9 +3,11 @@ package org.gokul;
 public class day111 extends Oct11 {
 	public void employeeId() {
 		System.out.println("Employe id is 123");
+	}
 	
-super.employeeId();
-
+	public void employeeName() {
+		System.out.println("Employee name is poo");
 	}
 }
+
 
